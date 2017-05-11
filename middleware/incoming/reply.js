4fc698e6509a8a75bebf4db1e7b6d0e992453973
Edward Ciggaar <edward_ciggaar@nl.ibm.com>
2017-05-11 17:30:34 +0200

    Search automatically for workspace

--- middleware/incoming/reply.js
+++ middleware/incoming/reply.js
@@ -19,36 +19,58 @@ const config = require('../../config');
 const watsonConversationStorageMiddleware = require('../watson_conversation_storage');
 const watsonConversation = watson.conversation(config.watsonConversationCredentials);
 
+let workspaceId = '';
+let workspaceFound = false;
+
 const replyToUser = {
   type: 'incoming',
   name: 'reply-to-user',
   controller: (bot, update, next) => {
-    watsonConversation.listWorkspaces( (err, watsonWorkspaces) => {
-      console.log(JSON.stringify(watsonWorkspaces));
-    });
 
-    const messageForWatson = {
-        context: update.session.context,
-        workspace_id: process.env.WATSON_WORKSPACE_ID,
-        input: {
-            text: update.message.text,
+    // By default picks the first workspace that is found in the list...
+    // Then a boolean is set to true so that this API call is called only once
+    if (!workspaceFound) {
+      watsonConversation.listWorkspaces( (err, watsonWorkspaces) => {
+        if (!err && watsonWorkspaces.workspaces[0]) {
+          workspaceId = watsonWorkspaces.workspaces[0].workspace_id;
+          console.log('Workspace ID being used....: ' + workspaceId);
+        } else { // In case of error or empty list
+          workspaceId = process.env.WATSON_WORKSPACE_ID;
         }
-    };
 
-    watsonConversation.message(messageForWatson, (err, watsonUpdate) => {
-        if (err) {
-          bot.sendTextCascadeTo(['Welcome! To complete the setup, follow the steps in the README.md to create your own workspace and link it to this application.',
-                                'Have fun!!'], update.sender.id);
-        } else {
-          watsonConversationStorageMiddleware.updateSession(update.sender.id, watsonUpdate);
-          const watsontext = watsonUpdate.output.text;
-          bot.sendTextCascadeTo(watsontext, update.sender.id);
-        }
-    });
+        workspaceFound = true;
+        submitMessage(bot, update);
+      });
+    }
+
+    if (workspaceFound) {
+      submitMessage(bot, update);
+    }
 
     next();
   }
 };
+
+function submitMessage(bot, update) {
+  const messageForWatson = {
+      context: update.session.context,
+      workspace_id: workspaceId,
+      input: {
+          text: update.message.text,
+      }
+  };
+
+  watsonConversation.message(messageForWatson, (err, watsonUpdate) => {
+      if (err) {
+        bot.sendTextCascadeTo(['Welcome! To complete the setup, follow the steps in the <a href="https://github.com/eciggaar/botmaster-watsonconversation-socketio#step-1-import-your-workspace">README.md</a> to create your own workspace and link it to this application.',
+                              'Have fun!!'], update.sender.id);
+      } else {
+        watsonConversationStorageMiddleware.updateSession(update.sender.id, watsonUpdate);
+        const watsontext = watsonUpdate.output.text;
+        bot.sendTextCascadeTo(watsontext, update.sender.id);
+      }
+  });
+}
 
 module.exports = {
   replyToUser
